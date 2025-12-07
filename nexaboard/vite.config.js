3af3e9af52b1549589ab17e43c12d44770b91177
osama <osamaammarothman@gmail.com>
2025-12-07 08:15:11 +0000

    oooo

--- nexaboard/vite.config.js
+++ nexaboard/vite.config.js
@@ -4,9 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    host:true,
-    port:5173
+  server: {
+    host: true 
+  },
+  preview: {
+    host: '0.0.0.0', 
+    port: 4173
   },
   plugins: [react(), tailwindcss()],
 });
